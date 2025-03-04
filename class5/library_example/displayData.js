e22eafef38a1b340f3e6717c01bedc5df96a830f
Dora Do <doradocodes@gmail.com>
2025-03-04 16:16:23 -0500

    Class 5 example

--- class5/library_example/displayData.js
+++ class5/library_example/displayData.js
@@ -1,191 +1,132 @@
-// const books = [
-//     {
-//         title: 'To Kill a Mockingbird',
-//         author: 'Harper Lee',
-//         numOfCopies: 12,
-//     },
-//     {
-//         title: '1984',
-//         author: 'George Orwell',
-//         numOfCopies: 7,
-//     },
-//     {
-//         title: 'The Great Gatsby',
-//         author: 'F. Scott Fitzgerald',
-//         numOfCopies: 5,
-//     },
-//     {
-//         title: 'The Catcher in the Rye',
-//         author: 'J.D. Salinger',
-//         numOfCopies: 9,
-//     },
-//     {
-//         title: 'Pride and Prejudice',
-//         author: 'Jane Austen',
-//         numOfCopies: 15,
-//     },
-//     {
-//         title: 'Moby-Dick',
-//         author: 'Herman Melville',
-//         numOfCopies: 8,
-//     },
-//     {
-//         title: 'War and Peace',
-//         author: 'Leo Tolstoy',
-//         numOfCopies: 6,
-//     },
-//     {
-//         title: 'The Lord of the Rings',
-//         author: 'J.R.R. Tolkien',
-//         numOfCopies: 20,
-//     },
-//     {
-//         title: 'Harry Potter and the Sorcerer\'s Stone',
-//         author: 'J.K. Rowling',
-//         numOfCopies: 25,
-//     },
-//     {
-//         title: 'The Hobbit',
-//         author: 'J.R.R. Tolkien',
-//         numOfCopies: 18,
-//     }
-// ];
-
-
+const libraryContainer = document.querySelector('.library');
 /*
-Part 1: Classes
+    Part 1: Classes
+    Create a Book class that has the following properties:
+    - element (HTMLElement)
+    - title (string)
+    - author (string)
+    - numCopies (number, the number of copies available)
 
-Create a Book class that has the following properties:
-- element (HTMLElement)
-- title (string)
-- author (string)
-- numCopies (number, the number of copies available)
-
-Add the following methods:
-- displayBook():
-    - Generates HTML to represent the book
+    Add the following methods:
+    - displayBook():
+        - Generates HTML to represent the book
 */
-
 class Book {
-    constructor(element, title, author, numOfCopies) {
+    constructor(element, title, author, numCopies) {
         this.element = element;
         this.title = title;
         this.author = author;
-        this.numOfCopies = numOfCopies;
-
-        this.element.classList.add('book');
-
-        const self = this;
-        this.element.addEventListener('click', function () {
-            self.borrow();
-        })
+        this.numCopies = numCopies;
     }
 
     displayBook() {
         this.element.innerHTML = '';
 
-        if (this.numOfCopies === 0) {
-            this.element.classList.add('disabled');
-        }
-
-        const titleElement = document.createElement('p')
+        // console.log(this.element, this.title, this.author, this.numCopies);
+        // this.element.innerHTML = `${this.title}, ${this.author}, ${this.numCopies}`;
+        this.element.classList.add('book');
+        
+        // create new title element
+        const titleElement = document.createElement('p');
         titleElement.classList.add('book_title');
-        titleElement.innerText = this.title;
+        titleElement.innerHTML = this.title;
 
+        // create a new author element
         const authorElement = document.createElement('p');
         authorElement.classList.add('book_author');
-        authorElement.innerText = this.author;
+        authorElement.innerHTML = this.author;
 
-        const numOfCopiesElement = document.createElement('p');
-        numOfCopiesElement.classList.add('book_num_copies');
-        numOfCopiesElement.innerText = 'Available copies: ' + this.numOfCopies;
+        // create a numberofCopies element
+        const numCopiesElement = document.createElement('p');
+        numCopiesElement.classList.add('book_num_copies');
+        numCopiesElement.innerHTML = "Available copies: " + this.numCopies;
 
         this.element.appendChild(titleElement);
         this.element.appendChild(authorElement);
-        this.element.appendChild(numOfCopiesElement);
+        this.element.appendChild(numCopiesElement);
     }
 
     borrow() {
-        if (this.numOfCopies > 0) {
-            this.numOfCopies--;
+        if (this.numCopies > 1) {
+            this.numCopies = this.numCopies - 1;
             this.displayBook();
+        } else {
+            this.numCopies = 0;
+            this.displayBook();
+            this.element.classList.add('disabled');
         }
     }
-
 }
 
-// const libraryElement = document.querySelector('.library');
-
-
-// for (let i = 0; i < books.length; i++) {
-//     const book1Element = document.createElement('div');
-//     libraryElement.appendChild(book1Element);
-//     const book1 = new Book(book1Element, books[i].title, books[i].author, books[i].numOfCopies);
-//     book1.displayBook();
+// for(let i = 0; i < books.length; i++) {
+//     const myBookElement = document.createElement('div');
+//     const myBook = new Book(myBookElement, books[i].title, books[i].author, books[i].numOfCopies);
+//     myBook.displayBook();
+//     myBookElement.addEventListener('click', () => {
+//         myBook.borrow();
+//     })
+//     libraryContainer.append(myBookElement)
 // }
 
-
-/* 
-Part 2: Encapsulation
-
-Add a function borrow():
-    - Decreases the number of copies by 1 and prints how many copies are left. 
-    - If there are no more copies left, add a class "disabled" to the element.
+/*
+    Part 2: Encapsulation
+    Add a function borrow():
+        - Decreases the number of copies by 1 and prints how many copies are left.
+        - If there are no more copies left, add a class "disabled" to the element.
 */
 
-/* 
-Part 3: Inheritance
-
-Create a new class Textbook which extends Book.
+/*
+    Part 3: Inheritance
+    Create a new class Textbook which extends Book.
 */
 
 class Textbook extends Book {
-    constructor(element, title, author, numOfCopies) {
-        super(element, title, author, numOfCopies);
+    constructor(element, title, author, numCopies) {
+        super(element, title, author, numCopies);
         this.element.classList.add('textbook');
     }
 }
+const myTextbookElement = document.createElement('div');
+const myTextbook = new Textbook(myTextbookElement, 'Textbook title', 'Textbook authors', 1);
+myTextbook.displayBook();
+myTextbookElement.addEventListener('click', () => {
+    myTextbook.borrow();
+})
+libraryContainer.append(myTextbookElement)
 
-// const textbookElement = document.createElement('div');
-// libraryElement.appendChild(textbookElement);
-// const textbook = new Textbook(textbookElement, 'Textbook title', 'Textbook author', 100);
-// textbook.displayBook();
-
-
-
-/* 
-Part 4: Polymorphism
-
-Create a new class Ebook which extends Book, and displays only the title and author.
+/*
+    Part 4: Polymorphism
+    Create a new class Ebook which extends Book, and displays only the title and author.
 */
 
 class Ebook extends Book {
     constructor(element, title, author) {
         super(element, title, author);
-        this.element.classList.add('ebook');
+        this.element.classList.add('ebook')
     }
 
     displayBook() {
         this.element.innerHTML = '';
 
-        if (this.numOfCopies === 0) {
-            this.element.classList.add('disabled');
-        }
-
-        const titleElement = document.createElement('p')
+        // console.log(this.element, this.title, this.author, this.numCopies);
+        // this.element.innerHTML = `${this.title}, ${this.author}, ${this.numCopies}`;
+        this.element.classList.add('book');
+        
+        // create new title element
+        const titleElement = document.createElement('p');
         titleElement.classList.add('book_title');
-        titleElement.innerText = this.title;
+        titleElement.innerHTML = this.title;
 
+        // create a new author element
         const authorElement = document.createElement('p');
         authorElement.classList.add('book_author');
-        authorElement.innerText = this.author;
+        authorElement.innerHTML = this.author;
 
         this.element.appendChild(titleElement);
         this.element.appendChild(authorElement);
     }
 }
-
-// const ebookElement = document.createElement('div');
-// libraryElement.appendChild(ebookElement);
-// const ebook = new Ebook(ebookElement, 'Ebook title', 'Ebook author');
-// ebook.displayBook();
+const myEbookElement = document.createElement('div');
+const myEbook = new Ebook(myEbookElement, 'Ebook title', 'Ebook author');
+myEbook.displayBook();
+libraryContainer.append(myEbookElement);
--- class5/library_example/fetchData.js
+++ class5/library_example/fetchData.js
@@ -2,97 +2,40 @@
     Part 5: Fetch the JSON data using fetch()
 */
 
+function getBookData() {
+    fetch('books.json')
+    .then(data => data.json())
+    .then(bookData => {
+        console.log(bookData.length);
+        for(let i = 0; i < bookData.length; i++) {
+            const myBookElement = document.createElement('div');
+            const myBook = new Book(myBookElement, bookData[i].title, bookData[i].author, bookData[i].numOfCopies);
+            myBook.displayBook();
+            myBookElement.addEventListener('click', () => {
+                myBook.borrow();
+            })
+            libraryContainer.append(myBookElement)
+        }
+    });
+}
 
-// function getBookData() {
-//     fetch('books.json')
-//         .then((response) => {
-//             console.log(response);
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log(data);
-//             const libraryElement = document.querySelector('.library');
-//             for (let i = 0; i < data.length; i++) {
-//                 const book1Element = document.createElement('div');
-//                 libraryElement.appendChild(book1Element);
-//                 const book1 = new Book(book1Element, data[i].title, data[i].author, data[i].numOfCopies);
-//                 book1.displayBook();
-//             }
-//         })
-// }
-
-// async function getBookData() {
-//     const response = await fetch('books.json');
-//     if (response) {
-//         console.log(response);
-//         const data = await response.json();
-//         console.log(data);
-
-//         const libraryElement = document.querySelector('.library');
-//         for (let i = 0; i < data.length; i++) {
-//             const book1Element = document.createElement('div');
-//             libraryElement.appendChild(book1Element);
-//             const book1 = new Book(book1Element, data[i].title, data[i].author, data[i].numOfCopies);
-//             book1.displayBook();
-//         }
-//     }
-// }
+async function getBookDataAsync() {
+    const data = await fetch('books.json');
+    const bookData = await data.json();
+    for(let i = 0; i < bookData.length; i++) {
+        const myBookElement = document.createElement('div');
+        const myBook = new Book(myBookElement, bookData[i].title, bookData[i].author, bookData[i].numOfCopies);
+        myBook.displayBook();
+        myBookElement.addEventListener('click', () => {
+            myBook.borrow();
+        })
+        libraryContainer.append(myBookElement)
+    }
+}
 
 // getBookData();
+getBookDataAsync();
 
 /*
     Part 6: Fetch data from Gutendex and create a new class ExternalBook that extends Book to represent it
 */
-
-const libraryElement = document.querySelector('.library');
-
-async function getGutendexData() {
-    const response = await fetch('gutendex.json');
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    // do what i want with data
-    for (let i = 0; i < data.results.length; i++) {
-        const bookEl = document.createElement('div');
-        libraryElement.appendChild(bookEl);
-        const book = new ExternalBook(bookEl, data.results[i].title, data.results[i].authors, data.results[i].formats);
-        book.displayBook();
-    }
-}
-
-class ExternalBook extends Book {
-    constructor(element, title, authors, formats) {
-        super(element, title, authors);
-
-        this.formats = formats;
-        this.element.classList.add('external_book');
-    }
-
-    displayBook() {
-        this.element.innerHTML = '';
-
-        if (this.numOfCopies === 0) {
-            this.element.classList.add('disabled');
-        }
-
-        const titleElement = document.createElement('p')
-        titleElement.classList.add('book_title');
-        titleElement.innerText = this.title;
-
-        this.element.appendChild(titleElement);
-
-        for (let i = 0; i < this.author.length; i++) {
-            const authorElement = document.createElement('p');
-            authorElement.classList.add('book_author');
-            console.log(this.author[i]);
-            authorElement.innerText = this.author[i].name;
-            this.element.appendChild(authorElement);
-        }
-
-        this.element.style.backgroundImage = `url("${this.formats['image/jpeg']}")`
-
-
-    }
-}
-
-getGutendexData();
